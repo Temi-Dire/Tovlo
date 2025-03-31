790a023b32ea005b3c8dba37ee883936145b018f
Temidire <temidireowoeye@gmail.com>
2025-03-31 11:37:07 +0100

    fix

--- src/component/Faq.tsx
+++ src/component/Faq.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { FaArrowDown } from "react-icons/fa";
 
 const Faq = () => {
-    const [show, setShow] = useState<number | null>(null)
+    // const [, setShow] = useState<number | null>(null)
   return (
     <section className="">
         <div className='flex justify-center font-playfair'>
@@ -11,17 +11,17 @@ const Faq = () => {
             </h1>
         </div>
         <div className="my-10">
-            {faqs.map(() => 
-            <div className="grid grid-cols-[auto_596px_auto]    px-10 py-5 border-y-2 border-primary ">
+            {faqs.map((item, i) => 
+            <div key={i} className={` grid grid-cols-[auto_650px_auto] px-10 py-5 border-y-2 border-primary `}>
                 <div className="flex items-star">
-                    <p className="text-[32px] m-0 font-playfair">01</p>
+                    <p className="text-[32px] m-0 font-playfair">{item.number}</p>
                 </div>
                 <div>
-                    <h2 className="font-medium italic text-[32px] font-playfair">Why should I feed my dog natural food</h2>
+                    <h2 className="font-medium italic text-[32px] font-playfair">{item.question}</h2>
                     {/* <p className="text-[20px] font-inter">TOVLO's natural Daily Dog Food is free from artificial additives, packed with real whole ingredients-no byproducts or processed meals, and mirrors what dogs thrive on in nature- keeping them healthier and happier.</p> */}
                 </div>
                 <div className="flex justify-end h-fit">
-                    <div className={`bg-[#D5E9D7] p-2 rounded-full cursor-pointer border-2 border-primary`} onClick={() => setShow()}>
+                    <div className={`bg-[#D5E9D7] p-2 rounded-full cursor-pointer border-2 border-primary`}>
                     {/* <div className={`bg-[#FBFFDC] p-2 rounded-full cursor-pointer border-2 border-primary`}> */}
                         {/* <FaArrowUp className="text-primary" /> */}
                         <FaArrowDown className="text-primary " />
